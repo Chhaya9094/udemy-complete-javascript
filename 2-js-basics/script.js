b3a7fd8cbb5dc1b77b25acb5bb79982d01306a97
Andrew Goss <andrewrgoss@gmail.com>
2017-05-08 16:49:06 -0400

    completed lectures: objects/properties, objects/methods

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -225,7 +225,7 @@ if (x === 5) {
 
 //////////////////////////////////////////////////////
 // Lecture: arrays
-
+/*
 var names = ['John', 'Jane', 'Mark'];
 var years = new Array(1990, 1969, 1948);
 
@@ -244,3 +244,92 @@ console.log(john);
 if (john.indexOf('teacher') === -1 ) {
     console.log('John is NOT a teacher.');
 }
+*/
+
+//////////////////////////////////////////////////////
+// Lecture: objects and properties
+// Objects = key-value pair. Group together different variables with no particular order. Each key-value pair is a property.
+/*
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
+
+//////////////////////////////////////////////////////
+// Lecture: objects and methods
+/*
+// v1.0
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        return 2017 - this.yearOfBirth;
+    }
+};
+
+// use the object's available method (function)
+//console.log(john.calculateAge(1990));
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age; //insert new key-value pair into object
+
+console.log(john);
+
+// v2.0
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        //return 2017 - this.yearOfBirth;
+        this.age = 2017 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
+console.log(john);
+
+var mike = {
+    yearOfBirth: 1950,
+    calculateAge: function() {
+        this.age = 2017 - this.yearOfBirth;
+    }
+};
+
+mike.calculateAge();
+console.log(mike);
+*/
+
