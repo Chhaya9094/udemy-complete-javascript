d8af9073fc91229c663455787d10cf062c87b5e0
Andrew Goss <andrewrgoss@gmail.com>
2017-05-09 17:12:52 -0400

    complete through coding challenge 2

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -333,3 +333,98 @@ mike.calculateAge();
 console.log(mike);
 */
 
+
+//////////////////////////////////////////////////////
+// Lecture: loops and iteration
+/*
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 10
+10, false, end loop!
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+// for loops
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+for (var i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
+
+// while loops
+var i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+    if (i === 3) {
+        break;
+    }
+}
+
+for (var i = 1; i <= 5; i++) {
+    if (i === 3) {
+        continue;
+    }
+    console.log(i);
+}
+*/
+
+
+//////////////////////////////////////////////////////
+// Coding Challenge 2
+/*
+CODING CHALLENGE 2
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
+
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
+
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from an array, like y[i], but also to set values in an array, like y[i] = ... You can also use the specific array methods.
+*/
+
+full_1 = printFullAge([1997, 2004, 1986]);
+console.log(full_1);
+full_2 = printFullAge([1992, 2013, 1946]);
+console.log(full_2);
+
+function calculateAge(year) {
+    var age = 2017 - year;
+    return age;
+}
+
+function printFullAge(yearsBorn) {
+    ages = [];
+    returnArray = [];
+    for (i = 0; i < yearsBorn.length; i++) {
+        age = calculateAge(yearsBorn[i]);
+        ages.push(age);
+    }
+    for (i = 0; i < ages.length; i++) {
+        if (ages[i] >= 18) {
+            returnArray.push(true);
+        } else {
+            returnArray.push(false);
+        }
+    }
+    return returnArray;
+}
+
