177f88ec3efbd2129e515d55c531dbe286b9eadd
Andrew Goss <andrewrgoss@gmail.com>
2017-05-22 17:15:44 -0400

    completed lectures: hoisting, scoping, execution stack/scope chain

--- 3-how-js-works/script.js
+++ 3-how-js-works/script.js
@@ -1,25 +1,36 @@
 ///////////////////////////////////////
-// Lecture: Hosting
+// Lecture: Hoisting
+/*
+// functions
+calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2017 - year);
+}
 
+// retirement(1956); --> this will not work with hoisting, as hoisting only works for function declarations. Hoisting can work with variables but in a different way.
 
+var retirement = function(year) {
+    console.log(65 - (2017 - year));
+}
 
+// variables
+console.log(age); // with hoisting code is scanned for variable declarations and these are initialized as undefined.
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    console.log(age);
+    var age = 65; // this uses function execution context object
+    console.log(age);
+}
+foo();
+console.log(age); // this uses global execution context object
+*/
 
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -38,10 +49,7 @@ function first() {
 }
 */
 
-
-
 // Example to show the differece between execution stack and scope chain
-
 /*
 var a = 'Hello!';
 first();
@@ -61,7 +69,6 @@ function third() {
     console.log(a + b + c + d);
 }
 */
-
 
 
 ///////////////////////////////////////
