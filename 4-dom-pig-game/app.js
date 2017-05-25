2fd9168ac2ec954f7f71329a5cd722aa7be584b0
Andrew Goss <andrewrgoss@gmail.com>
2017-05-25 15:56:21 -0400

    finished dom pig game app

--- 4-dom-pig-game/app.js
+++ 4-dom-pig-game/app.js
@@ -9,24 +9,12 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer;
+var score, roundScore, activePlayer, gamePlaying;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 1;
+init();
 
 //document.querySelector('#current-' + activePlayer).textContent = dice; // in order to select ids (like in css), use hash # symbol here [setter]
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent; // [getter]
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none'; // select a class with period . symbol here
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 /*
 function btn() {
@@ -36,15 +24,88 @@ btn();
 document.querySelector('.btn-roll').addEventListener('click', btn); // callback function - not called by us, but by another function
 */
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.btn-roll').addEventListener('click', function() { // anonymous function - no name so cannot be called outside of this expression (created at runtime)
+    if (gamePlaying) {
+        // 1. random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. display the result
-    var diceDOM = document.querySelector('.dice');
-    document.querySelector('.dice').style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        // 2. display the result
+        var diceDOM = document.querySelector('.dice');
+        document.querySelector('.dice').style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
-    // 3. update the round score IF the rolled number was NOT a 1
+        // 3. update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            // add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            // next player
+            nextPlayer();
+        }
+    }
+}); 
 
-}); // anonymous function - no name so cannot be called outside of this expression (created at runtime)
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        // add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+        //scores[activePlayer] = scores[activePlayer] + roundScore;
+
+        // update the UI 
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // check if player won the game
+        if (scores[activePlayer] >= 100) {
+            // player has won the game
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            // next player
+            nextPlayer();
+        }
+    }
+});
+
+function nextPlayer() {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // conditional ternary operator
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active'); // toggle = if class is not here, add it. If the class is here, remove it.
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none'; // select a class with period . symbol here
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
