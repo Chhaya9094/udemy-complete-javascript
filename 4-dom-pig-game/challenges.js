b18fa02193f4af7d62a1020c79d67961ac9ca76a
Andrew Goss <andrew.goss@groupeconnect.com>
2017-06-08 17:37:14 -0400

    completed coding challenge 1, coding challenge 2 in progress

--- 4-dom-pig-game/challenges.js
+++ 4-dom-pig-game/challenges.js
@@ -2,7 +2,7 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
@@ -11,12 +11,12 @@ CODING CHALLENGES
 
 Change the game to follow these rules:
 
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+1. A player loses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
-var score, roundScore, activePlayer, gamePlaying;
+var score, lastDice, roundScore, activePlayer, gamePlaying;
 
 init();
 
@@ -30,15 +30,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() { // an
         document.querySelector('.dice').style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
-        // 3. update the round score IF the rolled number was NOT a 1
-        if (dice !== 1) {
+        // 3. coding challenge - player loses entire score if two 6s in a row rolled --OR-- update the round score IF the rolled number was NOT a 1
+         if (dice === 6 && lastDice === 6) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0'; // update the UI 
+            nextPlayer();
+        } else if (dice !== 1) {
             // add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            prevRoll = dice; // store previous dice roll value in new variable
         } else {
             // next player
             nextPlayer();
         }
+
+        lastDice = dice;
     }
 }); 
 
@@ -66,22 +73,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     }
 });
 
-function nextPlayer() {
-    // next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // conditional ternary operator
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active'); // toggle = if class is not here, add it. If the class is here, remove it.
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
-}
-
-document.querySelector('.btn-new').addEventListener('click', init);
-
 function init() {
     scores = [0, 0];
     activePlayer = 0;
@@ -102,3 +93,20 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
+
+function nextPlayer() {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // conditional ternary operator
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active'); // toggle = if class is not here, add it. If the class is here, remove it.
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
