ce313be5908474bc31a34a56421c8e694d074ea5
Andrew Goss <andrewrgoss@gmail.com>
2017-09-13 17:35:36 -0400

    ui controller code in progress

--- 6-budget-app/app.js
+++ 6-budget-app/app.js
@@ -9,7 +9,15 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-    // some code
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector('.add__type').value, // will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('add__value').value
+            };
+        }
+    };
 
 })();
 
@@ -19,12 +27,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
 
-        console.log('It works.');
     }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
